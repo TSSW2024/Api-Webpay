a26daee85811251d20bc4cf476b69b9b96c8f5b0
JoseMalcaR <jmalca@utem.cl>
2024-07-08 00:12:27 -0400

    commit

--- api/config/config.go
+++ api/config/config.go
@@ -4,19 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 // DBURL genera la URL de conexión a la base de datos PostgreSQL
 func DBURL() string {
-	DBHost := os.Getenv("DB_HOST")
-	DBUser := os.Getenv("DB_USER")
-	DBPassword := os.Getenv("DB_PASSWORD")
-	DBPort := os.Getenv("DB_PORT")
-	DBName := os.Getenv("DB_NAME")
+	err := godotenv.Load(".env")
 
-	if DBHost == "" || DBUser == "" || DBPassword == "" || DBPort == "" || DBName == "" {
-		log.Fatal("Missing required environment variables for database connection")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
 	}
+
+	DBHost := os.Getenv("DBHost")
+	DBUser := os.Getenv("DBUser")
+	DBPassword := os.Getenv("DBPassword")
+	DBPort := os.Getenv("DBPort")
+	DBName := os.Getenv("DBName")
 
 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", DBUser, DBPassword, DBHost, DBPort, DBName)
 }
